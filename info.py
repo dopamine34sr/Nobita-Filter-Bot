8ef91dfe6f00c8b5b251021b25b5d188780c1330
dopamine34sr <168000337+dopamine34sr@users.noreply.github.com>
2024-04-24 20:21:13 +0530

    Update info.py

--- info.py
+++ info.py
@@ -22,13 +22,13 @@ BOT_TOKEN = environ.get('BOT_TOKEN')
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
 USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', True))
 
-PICS = (environ.get('PICS', 'https://te.legra.ph/file/c2c87b18586efc3036665.jpg https://te.legra.ph/file/c9478557da51154d47de5.jpg')).split()
-NOR_IMG = environ.get("NOR_IMG", "https://te.legra.ph/file/c2c87b18586efc3036665.jpg")
-MELCOW_VID = environ.get("MELCOW_VID", "https://te.legra.ph/file/c2c87b18586efc3036665.jpg")
+PICS = (environ.get('PICS', 'https://te.legra.ph/file/066b95c4b124a8d4daea8.jpg')).split()
+NOR_IMG = environ.get("NOR_IMG", "https://te.legra.ph/file/066b95c4b124a8d4daea8.jpg")
+MELCOW_VID = environ.get("MELCOW_VID", "https://te.legra.ph/file/066b95c4b124a8d4daea8.jpg")
 SPELL_IMG = environ.get("SPELL_IMG", "https://te.legra.ph/file/c2c87b18586efc3036665.jpg")
 
 # Admins, Channels & Users
-ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '6500072213').split()]
+ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '6500072213' '7013496553').split()]
 CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '').split()]
 auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '').split()]
 AUTH_USERS = (auth_users + ADMINS) if auth_users else []
@@ -43,7 +43,7 @@ SUPPORT_CHAT_ID = int(support_chat_id) if support_chat_id and id_pattern.search(
 NO_RESULTS_MSG = bool(environ.get("NO_RESULTS_MSG", True))
 
 # MongoDB information
-DATABASE_URI = environ.get("DATABASE_URI", "mongodb+srv://manrisky73:mohitmal123@cluster0.nzpuvrh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DATABASE_URI = environ.get("DATABASE_URI", "mongodb+srv://manrisky73:sr101123@cluster0.n05k2lw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 DATABASE_NAME = environ.get("DATABASE_NAME", "Cluster0")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
@@ -64,8 +64,8 @@ DELETE_CHANNELS = [int(dch) if id_pattern.search(dch) else dch for dch in enviro
 MAX_B_TN = environ.get("MAX_B_TN", "8")
 MAX_BTN = is_enabled((environ.get('MAX_BTN', "True")), True)
 PORT = environ.get("PORT", "8080")
-GRP_LNK = environ.get('GRP_LNK', 'https://t.me/gdhubnation_chat')
-CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/gdhubnation')
+GRP_LNK = environ.get('GRP_LNK', 'https://t.me/SWEETGUJJU')
+CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/SWEETGUJJU')
 MSG_ALRT = environ.get('MSG_ALRT', 'ꜱʜᴀʀᴇ  ᴀɴᴅ  ꜱᴜᴘᴘᴏʀᴛ  ᴜꜱ')
 LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002139378710'))
 SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'Support')
@@ -87,7 +87,7 @@ PROTECT_CONTENT = is_enabled((environ.get('PROTECT_CONTENT', "False")), False)
 PUBLIC_FILE_STORE = is_enabled((environ.get('PUBLIC_FILE_STORE', "True")), True)
 
 LANGUAGES = ["hindi", "hin", "tamil", "tam", "telugu", "tel", "english", "eng", "kannada", "kan", "malayalam", "mal"]
-TUTORIAL = environ.get('TUTORIAL', 'https://t.me/gdhubnation_chat')
+TUTORIAL = environ.get('TUTORIAL', 'https://t.me/SWEETGUJJU')
 IS_TUTORIAL = bool(environ.get('IS_TUTORIAL', True))
 
 LOG_STR = "Current Cusomized Configurations are:-\n"
